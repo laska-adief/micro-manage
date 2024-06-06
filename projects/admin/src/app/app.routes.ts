b95ca9be0615e25dd99f0629390748a68789893d
laska-adief <laskaadief.la@gmail.com>
2024-06-06 14:27:04 +0700

    [Micro Manage] add routing for admin app

--- projects/admin/src/app/app.routes.ts
+++ projects/admin/src/app/app.routes.ts
@@ -1,3 +1,30 @@
 import { Routes } from '@angular/router';
+import { PostComponent } from './pages/post/post.component';
+import { PostListComponent } from './pages/post/post-list/post-list.component';
+import { PostFormComponent } from './pages/post/post-form/post-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'post/list',
+    pathMatch: 'full',
+  },
+  {
+    path: 'post',
+    component: PostComponent,
+    children: [
+      {
+        path: 'list',
+        component: PostListComponent,
+      },
+      {
+        path: 'form',
+        component: PostFormComponent,
+      },
+      {
+        path: '**',
+        redirectTo: 'list',
+      },
+    ],
+  },
+];
