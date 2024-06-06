00f7bcc5cf9b7622b3c0e9925304a7b69273656f
laska-adief <laskaadief.la@gmail.com>
2024-06-06 14:21:17 +0700

    [Micro Manage] add exposes in webpack config admin

--- projects/admin/webpack.config.js
+++ projects/admin/webpack.config.js
@@ -33,6 +33,7 @@ module.exports = {
       filename: "remoteEntry.js",
       exposes: {
         "./Component": "./projects/admin/src/app/app.component.ts",
+        "./routes": "./projects/admin/src/app/app.routes.ts",
       },
 
       // For hosts (please adjust)
