e4dd621d5075d0fc7ce56841a5a8581c6bca3aaa
laska-adief <laskaadief.la@gmail.com>
2024-06-06 15:12:21 +0700

    [Micro Manage] create edit post function in PostService

--- projects/admin/src/app/services/post.service.ts
+++ projects/admin/src/app/services/post.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 import { PostProps } from '../types/post';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -10,11 +11,17 @@ export class PostService {
   readonly BASE_URL = 'https://jsonplaceholder.typicode.com/posts';
   constructor() {}
 
+  selectedPost = new BehaviorSubject<PostProps | null>(null);
+
   getPosts() {
     return this.http.get<PostProps[]>(this.BASE_URL);
   }
 
   addPost(post: PostProps) {
     return this.http.post<PostProps>(this.BASE_URL, post);
+  }
+
+  editPost(id: number, post: PostProps) {
+    return this.http.patch<PostProps>(this.BASE_URL + '/' + id, post);
   }
 }
